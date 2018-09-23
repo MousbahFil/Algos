38d4e918a3ac75e1a0098ec43ff850474622e3a7
quantummousbah@gmail.com <m@70684178>
2018-09-23 23:45:01 +0300

    Enhancing the LongestCommonPrefix implementation

--- src/main/java/com/mousbah/algorithms/LongestCommonPrefix.java
+++ src/main/java/com/mousbah/algorithms/LongestCommonPrefix.java
@@ -1,8 +1,8 @@
 /**
- *  Copyright Murex S.A.S., 2003-2018. All Rights Reserved.
- *
- *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
- *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
+ * Copyright Murex S.A.S., 2003-2018. All Rights Reserved.
+ * <p>
+ * This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
+ * express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
  */
 package com.mousbah.algorithms;
 
@@ -19,24 +19,21 @@ public class LongestCommonPrefix {
     //~ ----------------------------------------------------------------------------------------------------------------
 
     public static void main(String[] args) {
-        ArrayList<String> strings = new ArrayList<>(Arrays.asList("abcdefgh", "abcghijk", "abcefgh"));
+        ArrayList<String> strings = new ArrayList<>(Arrays.asList("abcdefgh", "abgghijk", "abcefgh","ab"));
         System.out.println(new LongestCommonPrefix().getLongestCommonPrefix(strings));
     }
 
     public String getLongestCommonPrefix(ArrayList<String> input) {
-//        List<String> sortedList = input.stream().sorted((s1, s2) -> s1.length() < s2.length() ? -1 : 1).collect(Collectors.toList());
-//        String prefix = sortedList.get(0);
-//        for (int i=1; i< input.size(); i++){
-//            for (int j=0; j)
-//        }
-        List<String> prefixes = getPrefixes(input.get(0));
-        String result = "";
-        for (String prefix : prefixes) {
-            if (isPrefix(prefix, input) && (prefix.length() > result.length())) {
-                result = prefix;
+        List<String> sortedList = input.stream().sorted((s1, s2) -> s1.length() < s2.length() ? -1 : 1).collect(Collectors.toList());
+        String prefix = sortedList.get(0);
+        for (int i = 1; i < prefix.length(); i++) {
+            for (int j = 1; j < sortedList.size(); j++) {
+                if (sortedList.get(j).charAt(i) != prefix.charAt(i)) {
+                    return prefix.substring(0, i);
+                }
             }
         }
-        return result;
+        return prefix;
     }
 
     private boolean isPrefix(String prefix, ArrayList<String> A) {
