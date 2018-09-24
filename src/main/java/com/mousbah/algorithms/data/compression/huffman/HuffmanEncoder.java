d6fbf2ed6b902693dab9096d15a8fba5d300ffd4
quantummousbah@gmail.com <m@70684178>
2018-09-24 21:03:41 +0300

    Implementing Huffman encoding

--- src/main/java/com/mousbah/algorithms/data/compression/huffman/HuffmanEncoder.java
+++ src/main/java/com/mousbah/algorithms/data/compression/huffman/HuffmanEncoder.java
@@ -1,8 +1,8 @@
 /**
- *  Copyright Murex S.A.S., 2003-2018. All Rights Reserved.
- *
- *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
- *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
+ * Copyright Murex S.A.S., 2003-2018. All Rights Reserved.
+ * <p>
+ * This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
+ * express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
  */
 package com.mousbah.algorithms.data.compression.huffman;
 
@@ -20,16 +20,23 @@ public class HuffmanEncoder {
     //~ Methods
     //~ ----------------------------------------------------------------------------------------------------------------
 
-    public static List<String> encode(String text) {
+    public static void main(String[] args) {
+        System.out.println(HuffmanEncoder.encode("ABAACDB"));
+    }
+
+    public static String encode(String text) {
         Map<Character, Integer> table = new HashMap<>();
 
         for (int i = 0; i < text.length(); i++) {
-            if (table.containsKey(text.charAt(i))) {
-                table.put(text.charAt(i), table.get(i) + 1);
+            char character = text.charAt(i);
+            if (table.containsKey(character)) {
+                table.put(character, table.get(character) + 1);
             } else {
-                table.put(text.charAt(i), 1);
+                table.put(character, 1);
             }
         }
+
+        System.out.println(table);
 
         PriorityQueue<AbstractHuffmanNode> priorityQueue = new PriorityQueue<>();
         Iterator<Map.Entry<Character, Integer>> iterator = table.entrySet().iterator();
@@ -38,6 +45,43 @@ public class HuffmanEncoder {
             priorityQueue.add(new LeafNode(entry.getValue(), entry.getKey()));
         }
 
+        while (priorityQueue.size() > 1) {
+            AbstractHuffmanNode rightNode = priorityQueue.poll();
+            AbstractHuffmanNode leftNode = priorityQueue.poll();
+            priorityQueue.add(new HuffmanNode(leftNode, rightNode));
+        }
+
+        Map<Character, String> codeTable = createHuffmanCodeTable((HuffmanNode) priorityQueue.poll());
+        System.out.println(String.format("The code table is '%s':", codeTable));
+
+        StringBuilder sb = new StringBuilder();
+        for (char character : text.toCharArray()) {
+            sb.append(codeTable.get(character));
+        }
+        return sb.toString();
+    }
+
+    private static Map<Character, String> createHuffmanCodeTable(HuffmanNode node) {
+        Map<Character, String> result = new HashMap<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        createHuffmanCodeTable(node, stringBuilder, result);
+        return result;
+    }
+
+    private static void createHuffmanCodeTable(AbstractHuffmanNode node, StringBuilder stringBuilder, Map<Character, String> map) {
+        if (node.isLeaf()) {
+            LeafNode leafNode = (LeafNode) node;
+            map.put(leafNode.getCharacter(), stringBuilder.toString());
+            return;
+        } else {
+            HuffmanNode huffmanNode = (HuffmanNode) node;
+            stringBuilder.append("0");
+            createHuffmanCodeTable(huffmanNode.getLeftNode(), stringBuilder, map);
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            stringBuilder.append("1");
+            createHuffmanCodeTable(huffmanNode.getRightNode(), stringBuilder, map);
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
     }
 
 }
